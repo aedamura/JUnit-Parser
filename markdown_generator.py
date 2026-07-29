64d7b93b4af100391e40c38e128fbcc7dbc44bc9
Adam Edamura <adamedamura@gmail.com>
2026-07-29 13:50:38 -0700

    add basic class and method markdown generation

--- markdown_generator.py
+++ markdown_generator.py
@@ -1,9 +1,99 @@
 from pathlib import Path
 
-from models import Project
+from models import Project, TestClass, TestFile, TestMethod
 
 
 class MarkdownGenerator:
 
-    def generate(self, project: Project, output_dir: Path):
-        ...
+    def generate(self, project: Project, output_dir: Path) -> None:
+
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        for test_file in project.test_files:
+
+            for test_class in test_file.classes:
+
+                self._write_class(test_file, test_class, output_dir)
+
+    def _write_class(self, test_file: TestFile, test_class: TestClass, output_dir: Path):
+        markdown = self._build_markdown(test_file, test_class)
+
+        output_path = output_dir / (
+            test_class.name + ".md"
+        )
+
+        output_path.write_text(markdown, encoding="utf-8")
+
+    def _build_markdown(self, test_file: TestFile, test_class: TestClass) -> str:
+        lines = []
+
+        lines.extend(self._get_title(test_class))
+
+        lines.extend(self._get_package(test_file))
+
+        lines.extend(self._get_methods(test_class))
+
+        return "\n".join(lines)
+
+    def _get_title(self, test_class: TestClass) -> list[str]:
+        lines = []
+
+        title = test_class.display_name or test_class.name
+
+        lines.append(f"# {title}")
+        lines.append("")
+        lines.append(f"**Class:** `{test_class.name}`")
+        lines.append("")
+
+        return lines
+
+    def _get_package(self, file: TestFile) -> list[str]:
+        lines = []
+
+        lines.append("## Package")
+        lines.append("")
+        lines.append(file.package or "(default)")
+        lines.append("")
+
+        return lines
+
+    def _get_methods(self, test_class: TestClass) -> list[str]:
+        lines = []
+
+        lines.append("## Test Methods")
+        for method in test_class.methods:
+            if not method.is_test:
+                continue
+
+            lines.extend(self._get_method(method))
+
+        return lines
+
+    def _get_method(self, method: TestMethod) -> list[str]:
+        lines = []
+
+        lines.append(f"### {method.display_name or method.name}")
+        lines.append("")
+
+        lines.append(f"- Method: `{method.name}`")
+        if method.tags:
+            lines.append(
+                "- Tags: "
+                + ", ".join(method.tags)
+            )
+
+        lines.append(
+            "- Disabled: "
+            + ("Yes" if method.is_disabled else "No")
+        )
+
+        if method.location:
+            lines.append(
+                f"- Source "
+                f"{Path(method.location.path).name}"
+                f":{method.location.line}"
+            )
+
+        lines.append("")
+
+        return lines
---
+++ tests/test_markdown_generator.py
@@ -0,0 +1,137 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(parent_dir)
+
+from markdown_generator import MarkdownGenerator
+from models import Project, TestFile, TestClass, SourceLocation, TestMethod
+
+def test_generator_creates_markdown_file(tmp_path):
+    project = Project(
+        test_files=[
+            TestFile(
+                path="UserTest.java",
+                package="com.example.users",
+                classes=[
+                    TestClass(
+                        name="UserTest",
+                        display_name="User Management Tests",
+                        methods=[
+                            TestMethod(
+                                name="shouldCreateUser",
+                                display_name="Creates a new user",
+                                is_test=True,
+                                tags=["user-management"],
+                                location=SourceLocation(
+                                    path="UserTest.java",
+                                    line=14
+                                )
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+    output_directory = tmp_path / "docs"
+
+    MarkdownGenerator().generate(project, output_directory)
+
+    markdown_file = output_directory / "UserTest.md"
+
+    assert markdown_file.exists()
+
+    content = markdown_file.read_text()
+
+    assert "# User Management Tests" in content
+    assert "**Class:** `UserTest`" in content
+    assert "## Test Methods" in content
+    assert "Creates a new user" in content
+    assert "user-management" in content
+    assert "UserTest.java:14" in content
+
+    print(content)
+
+def test_generator_handles_empty_test_class(tmp_path):
+
+    project = Project(
+        test_files=[
+            TestFile(
+                path="EmptyTest.java",
+                package="com.example",
+                classes=[
+                    TestClass(
+                        name="EmptyTest"
+                    )
+                ]
+            )
+        ]
+    )
+
+    output_dierctory = tmp_path / "docs"
+
+    MarkdownGenerator().generate(project, output_dierctory)
+
+    content = (
+        output_dierctory / "EmptyTest.md"
+    ).read_text()
+
+    assert "# EmptyTest" in content
+
+
+def test_generator_only_lists_tests(tmp_path):
+    project = Project(
+        test_files=[
+            TestFile(
+                path="UserTest.java",
+                package="com.example.users",
+                classes=[
+                    TestClass(
+                        name="UserTest",
+                        display_name="User Management Tests",
+                        methods=[
+                            TestMethod(
+                                name="shouldCreateUser",
+                                display_name="Creates a new user",
+                                is_test=True,
+                                is_disabled=False,
+                                tags=["user-management"],
+                                location=SourceLocation(
+                                    path="UserTest.java",
+                                    line=14
+                                )
+                            ),
+                            TestMethod(
+                                name="setup",
+                                display_name="Sets up unit tests",
+                                is_test=False,
+                                tags=["user-management"],
+                                lifecycle="BeforeEach",
+                                is_disabled=False,
+                                location=SourceLocation(
+                                    path="UserTest.java",
+                                    line=32
+                                )
+                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+    output_dierctory = tmp_path / "docs"
+
+    MarkdownGenerator().generate(project, output_dierctory)
+
+    content = (
+        output_dierctory / "UserTest.md"
+    ).read_text()
+
+
+    assert "shouldCreateUser" in content
+    assert not "setup" in content
