7433e9d64f3bcec48008e2bd4d0fb6bdd74f5983
Adam Edamura <adamedamura@gmail.com>
2026-07-30 00:49:59 -0700

    update parser tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,9 @@
 import os
 import sys
 from textwrap import dedent
-from unittest import result
+from pathlib import Path
+
+import pytest
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -10,174 +12,260 @@ sys.path.append(parent_dir)
 
 
 from parser import JavaParser
-from models import TestFile as SourceTestFile
+from models import TestClass, TestFile as SourceTestFile, TestMethod
 
+FILE_CONTENTS = dedent("""\
+    package com.example.users;
 
-def test_parser_reads_package_and_class(tmp_path):
-
-    java_file = tmp_path / "UserTest.java"
-
-    java_file.write_text(dedent("""\
-        package com.example.users;
-
-        import org.junit.jupiter.api.Test;
-        import org.junit.jupiter.api.Tag;
-        import org.junit.jupiter.api.DisplayName;
-
-        @DisplayName("User Management Tests")
-        class UserTest {
-
-            @Test
-            @Tag("user-management")
-            @DisplayName("Creates a new user")
-            void shouldCreateUser() {
-            }
-        }
-    """))
-
-    test_file = SourceTestFile(
-        path=str(java_file)
-    )
-
-    parser = JavaParser()
-
-    result = parser.parse(test_file)
-
-    # --------
-    # Package assertions
-    # --------
-
-    assert result.package == "com.example.users"
-
-    # --------
-    # Imports assertions
-    # --------
-
-    assert len(result.imports) == 3
-    assert result.imports == [
-        "org.junit.jupiter.api.Test",
-        "org.junit.jupiter.api.Tag",     
-        "org.junit.jupiter.api.DisplayName"   
-    ]
-
-    # --------
-    # Class assertions
-    # --------
-
-    assert len(result.classes) == 1
-    assert result.classes[0].name == "UserTest"
-    assert result.classes[0].display_name == "User Management Tests"
-    assert result.classes[0].annotations == ["DisplayName"]
-    assert result.classes[0].tags == []
-    assert result.classes[0].location.path == str(java_file) # type: ignore
-    assert result.classes[0].location.line == 8 # type: ignore
+    import org.junit.jupiter.api.Nested;
+    import org.junit.jupiter.api.Test;
+    import org.junit.jupiter.api.TestFactory;
+    import org.junit.jupiter.api.DisplayName;
+    import org.junit.jupiter.api.Tag;
     
+    import com.example.registration.RegistrationID;
 
-def test_parser_reads_methods_with_annotations(tmp_path):
+    @DisplayName("User class behaves correctly")
+    @Tag("feature")
+    class UserTest {
 
-    java_file = tmp_path / "UserTest.java"
+        @DisplayName("User Login Tests")
+        @Nested
+        @Tag("login")
+        @Tag("integration")
+        class LoginTests {
 
-    java_file.write_text(dedent("""\
-        package com.example.users;
-
-        import org.junit.jupiter.api.Test;
-        import org.junit.jupiter.api.Tag;
-        import org.junit.jupiter.api.DisplayName;
-
-        class UserTest {
-
+            @DisplayName("Correct credentials logs the user in")
             @Test
-            @Tag("user-management")
-            @DisplayName("Creates a new user")
-            void shouldCreateUser() {
+            @Tag("login")
+            void shouldLoginSuccessfully() {
             }
-        }
-    """))
 
-    test_file = SourceTestFile(
-        path=str(java_file)
-    )
+            @DisplayName("Users are limited to 5 incorrect password guesses")
+            @ParameterizedTest
+            @Tag("login")
+            void hasLimitedPasswordAttempts(){
+            }
 
-    parser = JavaParser()
-
-    result = parser.parse(test_file)
-
-    assert len(result.classes) == 1
-    assert result.classes[0].name == "UserTest"
-    assert len(result.classes[0].methods) == 1
-    assert result.classes[0].methods[0].name == "shouldCreateUser"
-    assert result.classes[0].methods[0].annotations == ["Test", "Tag", "DisplayName"]
-    assert result.classes[0].methods[0].tags == ["user-management"]
-    assert result.classes[0].methods[0].display_name == "Creates a new user"
-    assert result.classes[0].methods[0].location.path == str(java_file) # type: ignore
-    assert result.classes[0].methods[0].location.line == 12 # type: ignore
-
-def test_parser_reads_nested_classes(tmp_path):
-
-    java_file = tmp_path / "UserTest.java"
-
-    java_file.write_text(dedent("""\
-        package com.example.users;
-
-        import org.junit.jupiter.api.Nested;
-        import org.junit.jupiter.api.Test;
-
-        class UserTest {
-
+            @DisplayName("Tests for invalid logins")
             @Nested
-            class LoginTests {
+            class InvalidLoginTests {
 
+                @DisplayName("Incorrect credentials do not log the user in")
                 @Test
-                void shouldLoginSuccessfully() {
-                }
-
-                @Nested
-                class InvalidLoginTests {
-
-                    @Test
-                    void shouldFailWithInvalidCredentials() {
-                    }
-                }
-            }
-
-            class RegistrationTests {
-
-                @Test
-                void shouldRegisterSuccessfully() {
+                @Tag("login")
+                void shouldFailWithInvalidCredentials() {
                 }
             }
         }
-    """))
 
-    test_file = SourceTestFile(
-        path=str(java_file)
-    )
+        @DisplayName("User Registration Tests")
+        @Nested
+        @Tag("registration")
+        class RegistrationTests {
 
-    parser = JavaParser()
+            @DisplayName("User registers correctly")
+            @ParameterizedTest
+            @Tag("registraton")
+            void shouldRegisterSuccessfully(User user, RegistrationID id) {
+            }
+        }
 
-    result = parser.parse(test_file)
+        @DisplayName("Helper Methods")
+        class HelperMethods{
+        
+            @DisplayName("Produces the next userID")
+            @Test
+            void getUserID(){
+            }
+
+        }
+
+        @DisplayName("Generate User Credentials")
+        @BeforeEach
+        @Tag("initialization")
+        void initializeCredentials(){
+        }
+    }
+""")
+
+# -----------------
+# Helper Methods
+# -----------------
+
+def _initialize_result(tmp_path) -> SourceTestFile:
+    java_file = tmp_path / "UsersTest.java"
+    java_file.write_text(FILE_CONTENTS)
+    return JavaParser().parse(SourceTestFile(path=java_file))
+
+def _get_top_level_class(test_file: SourceTestFile) -> TestClass:
+    return test_file.classes[0]
+
+def _get_class(class_name: str, class_list: list[TestClass]) -> TestClass:
+    classes = []
+    classes.extend(class_list)
+
+    while len(classes) != 0:
+        cls = classes.pop(0)
+        classes.extend(cls.nested_classes)
+        if cls.name == class_name:
+            return cls
+
+    return TestClass(name="Not Found")
+
+def _get_method(method_name: str, cls: TestClass) -> TestMethod:
+    for method in cls.methods:
+        if method.name == method_name:
+            return method
+
+    return TestMethod(name="Not Found")
+
+# ----------
+# Test Packages and Imports
+# ----------
+
+def test_parser_reads_package(tmp_path):
+    result = _initialize_result(tmp_path)
+
+    assert result.package == "com.example.users" 
+
+def test_parser_reads_imports(tmp_path):
+    result = _initialize_result(tmp_path)
+
+    assert "com.example.registration.RegistrationID" in result.imports
+    assert "org.junit.jupiter.api.Tag" in result.imports
+    assert "org.junit.jupiter.api.Test" in result.imports
+    assert "org.junit.jupiter.api.DisplayName" in result.imports
+
+# ----------
+# Test Class Level Parsing
+# ----------
+
+def test_parser_reads_top_level_class(tmp_path):
+    result = _initialize_result(tmp_path)
 
     assert len(result.classes) == 1
     assert result.classes[0].name == "UserTest"
 
-    root = result.classes[0]
+def test_parser_reads_tagged_nested_classes(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
 
-    assert len(root.nested_classes) == 1
-    nested = root.nested_classes[0]
-    assert nested.name == "LoginTests"
-    assert nested.annotations == ["Nested"]
-    assert len(nested.methods) == 1
-    assert nested.methods[0].name == "shouldLoginSuccessfully"
-    assert nested.methods[0].annotations == ["Test"]
-    assert nested.methods[0].location.path == str(java_file) # type: ignore
-    assert nested.methods[0].location.line == 12 # type: ignore
+    assert len(cls.nested_classes) == 2
 
-    assert len(nested.nested_classes) == 1
-    sub_nested = nested.nested_classes[0]
-    assert sub_nested.name == "InvalidLoginTests" 
-    assert sub_nested.annotations == ["Nested"]
-    assert len(sub_nested.methods) == 1
-    assert sub_nested.methods[0].name == "shouldFailWithInvalidCredentials"
-    assert sub_nested.methods[0].annotations == ["Test"]
-    assert sub_nested.methods[0].location.path == str(java_file) # type: ignore
-    assert sub_nested.methods[0].location.line == 19 # type: ignore
+    nested = cls.nested_classes
+
+    assert any(
+        cls.name == "LoginTests"
+        for cls in nested
+    )
+    
+    assert any(
+        cls.name == "RegistrationTests"
+        for cls in nested
+    )
+
+    assert not any(
+        cls.name == "InvalidLoginTests"
+        for cls in nested
+    )
+
+    assert not any(
+        cls.name == "HelperMethods"
+        for cls in nested
+    )
+
+def test_parser_follows_nested_classes_tree(tmp_path):
+    result = _initialize_result(tmp_path)
+
+    assert len(result.classes) == 1
+    assert len(result.classes[0].nested_classes) == 2
+    assert result.classes[0].nested_classes[0].name == "LoginTests"
+    assert len(result.classes[0].nested_classes[0].nested_classes) == 1
+    assert result.classes[0].nested_classes[0].nested_classes[0].name == "InvalidLoginTests"
+    assert len(result.classes[0].nested_classes[1].nested_classes) == 0
+
+def test_parser_captures_correct_class_tags(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+
+    assert len(cls.tags) == 1
+    assert "feature" in cls.tags
+
+    cls2 = _get_class("LoginTests", result.classes)
+
+    assert len(cls2.tags) == 2
+    assert "login" in cls2.tags
+    assert "integration" in cls2.tags
+
+
+def test_parser_captures_correct_class_annotations(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+
+    assert "DisplayName" in cls.annotations
+    assert "Tag" in cls.annotations
+
+    cls = _get_class("LoginTests", result.classes)
+
+    assert "Nested" in cls.annotations
+    assert "DisplayName" in cls.annotations
+    assert "Tag" in cls.annotations
+
+def test_parser_captures_class_methods(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+
+    assert len(cls.methods) == 1
+
+    cls = _get_class("LoginTests", result.classes)
+
+    assert len(cls.methods) == 2
+
+def test_parser_captures_class_location(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+
+    assert cls.location
+    assert cls.location.line == 13
+
+    cls = _get_class("LoginTests", result.classes)
+
+    assert cls.location
+    assert cls.location.line == 19
+
+
+# ----------
+# Test Class Level Parsing
+# ----------
+
+def test_parser_captures_method_display_name(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+    method = _get_method("initializeCredentials", cls)
+
+    assert method.display_name == "Generate User Credentials"
+
+def test_parser_captures_method_annotations(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+    method = _get_method("initializeCredentials", cls)
+
+    assert "DisplayName" in method.annotations
+    assert "BeforeEach" in method.annotations
+    assert "Tag" in method.annotations
+
+def test_parser_captures_method_tags(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+    method = _get_method("initializeCredentials", cls)
+
+    assert "initialization" in method.tags
+
+def test_parser_captures_method_location(tmp_path):
+    result = _initialize_result(tmp_path)
+    cls = _get_class("UserTest", result.classes)
+    method = _get_method("initializeCredentials", cls)
+
+    assert method.location
+    assert method.location.line == 70
